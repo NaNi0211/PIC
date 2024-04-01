61398141859e5ce465917fd070c45a600c0ef043
NaNi0211 <148693233+NaNi0211@users.noreply.github.com>
2024-04-01 20:36:46 +0200

    Update DecodeDraft.java PC fehlerbehebung,gotobefehl

--- DecodeDraft.java
+++ DecodeDraft.java
@@ -1,18 +1,53 @@
 
+
+
 public class DecodeDraft {
 
     /*
      * name : binaryCode? ganze ram implementieren? NOP_befehl? quarzfreuez zur
      * einstellung der laufzeu flags nur bei arimetischen
+     * PCLuPCLATH
      */
-    int wRegister = 0;
-    int[] ram = new int[12];
-    int[] EEROM;
-    int pC;
-    int[] stack = new int[8];
-    int carrybit;
-    int digitcarrybit;
-    int zerobit;
+    private int wRegister = 0;
+    private int[] ram = new int[12];
+    private int[] EEROM;
+    private int pC;
+    private int[] stack = new int[8];
+    private int carrybit;
+    private int digitcarrybit;
+    private int zerobit;
+
+    public int getwRegister() {
+        return wRegister;
+    }
+
+    public int[] getRam() {
+        return ram;
+    }
+
+    public int[] getEEROM() {
+        return EEROM;
+    }
+
+    public int getpC() {
+        return pC;
+    }
+
+    public int[] getStack() {
+        return stack;
+    }
+
+    public int getCarrybit() {
+        return carrybit;
+    }
+
+    public int getDigitcarrybit() {
+        return digitcarrybit;
+    }
+
+    public int getZerobit() {
+        return zerobit;
+    }
 
     public void literalbefehl(int binaryCode) {
 
@@ -27,17 +62,20 @@ public class DecodeDraft {
             zero(result);
             carry(result, wRegister);
             digitcarry(result, bottomHalfbyte);
-
+            wRegister = result % 256;
+            pC++;
             break;
         case 0b0011_1001_0000_0000:// ANDLW
             result = wRegister & literalCode;
             zero(wRegister);
-
+            wRegister = result % 256;
+            pC++;
             break;
         case 0b0011_1000_0000_0000:// IORLW
             result = wRegister | literalCode;
             zero(result);
-
+            wRegister = result % 256;
+            pC++;
             break;
         case 0b0011_1100_0000_0000:// SUBLW
             result = wRegister - literalCode;
@@ -47,38 +85,40 @@ public class DecodeDraft {
             carry(result, wRegister);
             digitcarry(result, bottomHalfbyte);
             result *= -1;
-
+            wRegister = result % 256;
+            pC++;
             break;
         case 0b0011_0000_0000_0000:// MOVLW
             result = literalCode;
-
+            wRegister = result % 256;
+            pC++;
             break;
         case 0b0011_1010_0000_0000:// XORLW
             result = wRegister ^ literalCode;
             zero(result);
+            wRegister = result % 256;
+            pC++;
             break;
         default:
-
+            sprungbefehl(binaryCode);
         }
-
-        wRegister = result % 256;
-        pC++;
+        
+       
     }
 
     public void sprungbefehl(int binaryCode) {
 
         int instructionCode = binaryCode & 0b0011_1000_0000_0000;
-        int literalcode = binaryCode & 0b0000_0111_1111_1111;
+        int  literalCode = binaryCode & 0b0000_0111_1111_1111;
         switch (instructionCode) {
-        case 0b0010_1000_0000_0000: // GOTO
-           
+        case 0b0010_1000_0000_0000: // GOT
+            pC= literalCode;
             break;
         default:
 
         }
-        pC++;
+        
     }
-    
 
     private void digitcarry(int result, int halfbyte) {
         if ((halfbyte & 1111_0000) != 0) {
@@ -107,10 +147,10 @@ public class DecodeDraft {
     private void zero(int result) {
         if (result == 0) {
             zerobit = 1;
-            ram[3] = (ram[3]) | (zerobit << 5);
+            ram[3] = (ram[3]) | (zerobit << 2);
         } else {
             zerobit = 0;
-            ram[3] = (ram[3]) & (zerobit << 5);
+            ram[3] = (ram[3]) & (zerobit << 2);
         }
 
     }
