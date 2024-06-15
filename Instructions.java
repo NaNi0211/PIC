852cee1aa61aa3a75cc51af82c28a4fa2e4e8a21
NaNi0211 <148693233+NaNi0211@users.noreply.github.com>
2024-06-15 11:56:56 +0200

    Update Instructions.java

--- Instructions.java
+++ Instructions.java
@@ -1,13 +1,21 @@
-
+/*
+ * Sunlw test1
+ * 
+ * Bank test3
+ * 
+ * PD TO Status
+ * 
+ * CarryFlag test4
+ */
 public class Instructions extends DecodeDraft {
 
     // Literal
     public static void addlw(int literalCode) {
-        int result = lz(wRegister) + lz(literalCode);
-        int bottomHalfbyte = (wRegister & 0b0000_1111) + (literalCode & 0b0000_1111);
+        int result = wRegister + literalCode;
+        // int bottomHalfbyte = (wRegister & 0b0000_1111) + (literalCode & 0b0000_1111);
         zero(result);
-        carry(result, wRegister);
-        digitcarry(result, bottomHalfbyte);
+        carry(literalCode, wRegister, 0);
+        digitcarry(literalCode, wRegister, 0);
 
         wRegister = result % 256;
         pC_Next++;
@@ -27,33 +35,36 @@ public class Instructions extends DecodeDraft {
         pC_Next++;
     }
 
+    
     public static void sublw(int literalCode) {
-        int result = lz(wRegister) - lz(literalCode);
-        int zweierKom = (((literalCode ^ (0b1111_1111)) + 1) & 0b0000_1111);
-        int bottomHalfbyte = (wRegister & 0b0000_1111) + zweierKom;
+        int newW_Reg = lz(-wRegister);
+        int result = literalCode + newW_Reg;
+
+        // int bottomHalfbyte = (wRegister & 0b0000_1111) - ( result & 0b0000_1111);
         zero(result);
-        carry(result, wRegister);
-        digitcarry(result, bottomHalfbyte);
-        result *= -1;
+        carry(literalCode, newW_Reg, 1);
+        digitcarry(literalCode, newW_Reg, 1);
+
         wRegister = result % 256;
         pC_Next++;
     }
-private static int lz(int comp) {
-    while(comp<0) {
-        comp +=256;
+
+    public static int lz(int comp) {
+
+        while (comp < 0) {
+            comp += 256;
+        }
+        /*
+         * if(comp<0) {
+         * 
+         * comp =(comp ^ 0b1111_1111) ;
+         * System.out.println(Integer.toBinaryString(comp)); comp = (comp +1)|
+         * 0b1000_0000; System.out.println(Integer.toBinaryString(comp)); comp = comp
+         * &0b1111_1111; }
+         */
+        return comp;
     }
-    /*
-    if(comp<0) {
-        
-        comp =(comp ^ 0b1111_1111)  ;
-        System.out.println(Integer.toBinaryString(comp));
-        comp = (comp +1)| 0b1000_0000;
-        System.out.println(Integer.toBinaryString(comp));
-        comp = comp &0b1111_1111;
-    }
-    */
-    return comp;
-}
+
     public static void movlw(int literalCode) {
         int result = literalCode;
         wRegister = result % 256;
@@ -89,6 +100,7 @@ private static int lz(int comp) {
 
     public static void clrw() {
         wRegister = 0;
+        zero(wRegister);
         pC_Next++;
     }
 
@@ -99,10 +111,11 @@ private static int lz(int comp) {
     public static void setCallnReturn(int value) {
         if (value == 0) {
             stack[stackpointer++] = ++pC_Next;
+            stackpointer %=8;
         } else {
             pC_Next = stack[--stackpointer];
         }
-     
+
     }
 
     // byteorientated
@@ -124,14 +137,8 @@ private static int lz(int comp) {
         pC_Next++;
     }
 
-    public static void clrw(int fileCode) {
-        wRegister = 0;
-        zero(wRegister);
-        pC_Next++;
-    }
-
     public static void comf(int fileCode, int destBit) {
-        int result = (ram[rb0][fileCode]^0b1111_1111) % 256;
+        int result = (ram[rb0][fileCode] ^ 0b1111_1111) % 256;
         zero(result);
         if (destBit != 0) {
             ram[rb0][fileCode] = result;
@@ -142,7 +149,7 @@ private static int lz(int comp) {
     }
 
     public static void decf(int fileCode, int destBit) {
-        int result = lz(ram[rb0][fileCode]-1)% 256;
+        int result = lz(ram[rb0][fileCode] - 1) % 256;
         zero(result);
         if (destBit != 0) {
             ram[rb0][fileCode] = result;
@@ -153,7 +160,7 @@ private static int lz(int comp) {
     }
 
     public static void incf(int fileCode, int destBit) {
-        int result = ++ram[rb0][fileCode]% 256;
+        int result = ++ram[rb0][fileCode] % 256;
         zero(result);
         if (destBit != 0) {
             ram[rb0][fileCode] = result;
@@ -164,7 +171,7 @@ private static int lz(int comp) {
     }
 
     public static void iorwf(int fileCode, int destBit) {
-        int result = (ram[rb0][fileCode] | wRegister)% 256;
+        int result = (ram[rb0][fileCode] | wRegister) % 256;
         zero(result);
         if (destBit != 0) {
             ram[rb0][fileCode] = result;
@@ -175,7 +182,7 @@ private static int lz(int comp) {
     }
 
     public static void xorwf(int fileCode, int destBit) {
-        int result = (ram[rb0][fileCode] ^ wRegister)% 256;
+        int result = (ram[rb0][fileCode] ^ wRegister) % 256;
         zero(result);
         if (destBit != 0) {
             ram[rb0][fileCode] = result;
@@ -186,20 +193,64 @@ private static int lz(int comp) {
     }
 
     public static void subwf(int fileCode, int destBit) {
-        int result = (lz(ram[rb0][fileCode]) - lz(wRegister))% 256;
+        int newW_Reg = lz(-wRegister);
+        int result = lz(ram[rb0][fileCode] -wRegister) ;
         zero(result);
+
         if (destBit != 0) {
+            carry(ram[rb0][fileCode], newW_Reg, 1);
+            digitcarry(ram[rb0][fileCode], newW_Reg, 1);
             ram[rb0][fileCode] = result;
+
         } else {
+            carry(ram[rb0][fileCode], newW_Reg, 1);
+            digitcarry(ram[rb0][fileCode], newW_Reg, 1);
             wRegister = result;
+
         }
         pC_Next++;
     }
+
+    public static void rlf(int fileCode, int destBit) {
+
+        int oldCarry = carrybit;
+
+        carrybit = (ram[rb0][fileCode] & 0b1000_0000) >> 7;
+
+        if (destBit != 0) {
+            ram[rb0][fileCode] = ((ram[rb0][fileCode] << 1) | oldCarry) % 256;
+        } else {
+            wRegister = ((ram[rb0][fileCode] << 1) | oldCarry) % 256;
+        }
+
+        ram[rb0][3] = ram[rb0][3] | carrybit;
+
+        pC_Next++;
+    }
+    public static void rrf(int fileCode, int destBit) {
+
+        int oldCarry = carrybit;
+
+        carrybit = (ram[rb0][fileCode] & 0b0000_0001);
+
+        if (destBit != 0) {
+            ram[rb0][fileCode] = ((ram[rb0][fileCode] >> 1) |(oldCarry<<7)) % 256;
+        } else {
+            wRegister = ((ram[rb0][fileCode] >> 1) | (oldCarry<<7)) % 256;
+        }
+
+        ram[rb0][3] = ram[rb0][3] | carrybit;
+
+        pC_Next++;
+    }
+    
+   
+    
     // Ant
 
     public static void movwf(int fileCode) {
 
-        ram[rb0][fileCode] = wRegister% 256;
+        ram[rb0][fileCode] = wRegister % 256;
         pC_Next++;
 
     }
@@ -207,13 +258,16 @@ private static int lz(int comp) {
     public static void addwf(int fileCode, int destBit) {
 
         int fileValue = ram[rb0][fileCode];
-        int result = (wRegister + fileValue)% 256;
+        int result = (wRegister + fileValue) % 256;
         zero(result);
-        carry(result, wRegister);
-        digitcarry(result, (wRegister & 0b0000_1111) + (fileValue & 0b0000_1111));
+
         if (destBit != 0) {
+            carry( fileValue,wRegister, 0);
+            digitcarry( fileValue,wRegister,  0);
             ram[rb0][fileCode] = result;
         } else {
+            carry( fileValue,wRegister, 0);
+            digitcarry( fileValue,wRegister,  0);
             wRegister = result;
         }
         pC_Next++;
@@ -222,7 +276,7 @@ private static int lz(int comp) {
     public static void andwf(int fileCode, int destBit) {
 
         int fileValue = ram[rb0][fileCode];
-        int result = (wRegister & fileValue)% 256;
+        int result = (wRegister & fileValue) % 256;
         zero(result);
         if (destBit != 0) {
             ram[rb0][fileCode] = result;
@@ -234,23 +288,25 @@ private static int lz(int comp) {
     }
 
     public static void swapf(int fileCode, int destBit) {
-        int upperresult = (ram[rb0][fileCode] & 0b0000_1111) << 8;
-        int lowerresult = (ram[rb0][fileCode] & 0b1111_0000) >> 8;
+        int upperresult = (ram[rb0][fileCode] & 0b0000_1111) << 4;
+        int lowerresult = (ram[rb0][fileCode] & 0b1111_0000) >> 4;
 
-        int result = (upperresult | lowerresult)% 256;
+        int result = (upperresult | lowerresult) % 256;
 
         if (destBit != 0) {
             ram[rb0][fileCode] = result;
         } else {
             wRegister = result;
         }
+        pC_Next++;
     }
 
     public static void decfsz(int fileCode) {
 
-        ram[rb0][fileCode]=(lz(--ram[rb0][fileCode]))%256;
+        ram[rb0][fileCode] = (lz(--ram[rb0][fileCode])) % 256;
         if (ram[rb0][fileCode] == 0) {
             pC_Next += 2; // Skip next instruction
+            runtime += (4.0 / quartz);
         } else {
             pC_Next++;
         }
@@ -258,9 +314,10 @@ private static int lz(int comp) {
 
     public static void incfsz(int fileCode) {
 
-        ram[rb0][fileCode]=(++ram[rb0][fileCode])%256;
+        ram[rb0][fileCode] = (++ram[rb0][fileCode]) % 256;
         if (ram[rb0][fileCode] == 0) {
             pC_Next += 2; // Skip next instruction
+            runtime += (4.0 / quartz);
         } else {
             pC_Next++;
         }
@@ -282,7 +339,7 @@ private static int lz(int comp) {
 
         if ((ram[rb0][fileCode] & (1 << bitCode)) == 0) {
             pC_Next += 2;
-
+            runtime += (4.0 / quartz);
             // Skip next instruction
         } else {
             pC_Next++;
@@ -293,10 +350,11 @@ private static int lz(int comp) {
 
         if ((ram[rb0][fileCode] & (1 << bitCode)) != 0) {
             pC_Next += 2;
-
+            runtime += (4.0 / quartz);
             // Skip next instruction
         } else {
             pC_Next++;
         }
     }
+
 }
